385973ac4148e28893cb4b281fde1363f2308f85
Kelvin Osasogie <kosaizeko@gmail.com>
2026-08-12 08:55:36 +0100

    vercel error correctiom 2

--- client/src/utils/pushNotification.js
+++ client/src/utils/pushNotification.js
@@ -1,3 +1,55 @@
+// const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY
+
+// export async function subscribeToPush() {
+//     if (!('serviceWorker' in navigator)) {
+//         throw new Error('Service workers are not supported')
+//     }
+
+//     if (!('PushManager' in window)) {
+//         throw new Error('Push notifications are not supported')
+//     }
+
+//     const permission = await Notification.requestPermission()
+
+//     if (permission !== 'granted') {
+//         throw new Error('Notification permission was not granted')
+//     }
+
+//     const registration = await navigator.serviceWorker.ready
+
+//     const subscription = await registration.pushManager.subscribe({
+//         userVisibleOnly: true,
+//         applicationServerKey: VAPID_PUBLIC_KEY
+//     })
+
+//     // Send the subscription to Journex
+//     const response = await api.post(
+//         '/notifications/subscribe',
+//         {
+           
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             credentials: 'include',
+//             body: JSON.stringify({
+//                 subscription
+//             })
+//         }
+//     )
+
+//     if (!response.ok) {
+//         throw new Error('Failed to save push subscription')
+//     }
+
+//     return subscription
+// }
+
+
+
+
+
+import api from "../api"
+
 const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY
 
 export async function subscribeToPush() {
@@ -23,25 +75,7 @@ export async function subscribeToPush() {
     })
 
     // Send the subscription to Journex
-    const response = await fetch(
-        'http://localhost:3000/notifications/subscribe',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-                subscription
-            })
-        }
-    )
-
-    if (!response.ok) {
-        throw new Error('Failed to save push subscription')
-    }
+    await api.post('/notifications/subscribe', { subscription })
 
     return subscription
 }
-
-
